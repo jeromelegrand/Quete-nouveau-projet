c929b93dd6c2c2fb8b261cc37639c49e1a299d97
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-29 12:24:52 +0200

    ajout controller review et des deux routes index et new

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Review;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Review controller.
+ *
+ * @Route("review")
+ */
+class ReviewController extends Controller
+{
+    /**
+     * Lists all review entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
+    }
+
+    /**
+     * Creates a new review entity.
+     *
+     * @Route("/new", name="review_new")
+     * @Method("GET")
+     */
+    public function newAction(Request $request)
+    {
+        $review = new Review();
+
+
+
+        return $this->render('review/new.html.twig', array(
+            'review' => $review,
+        ));
+    }
+}
